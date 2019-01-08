fcc436f2cb64d882e7db09076014cf4bb2bb4b85
ozzy <ozzy@cybertekschool.com>
2019-01-08 11:37:56 -0500

    jdbc test cases

--- src/test/java/com/bookit/tests/JDBCTest.java
+++ src/test/java/com/bookit/tests/JDBCTest.java
@@ -2,7 +2,9 @@ package com.bookit.tests;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.testng.annotations.Test;
 
@@ -15,8 +17,29 @@ public class JDBCTest {
 	@Test
 	public void PostGreSQL() throws SQLException {
 		Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-			
+		Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);;
+		ResultSet resultset = statement.executeQuery("Select * from countries");
 		
+		// next method -> move pointer to next row
+//		resultset.next();
+//		System.out.println(resultset.getString(1)+"-"+resultset.getString("country_name")+"-"+resultset.getInt(3));
+
+//		while(resultset.next()) {
+//			System.out.println(resultset.getString(1)+"-"+resultset.getString("country_name")+"-"+resultset.getInt(3));
+//
+//		}
+		resultset.next();
+		resultset.next();
+		resultset.next();
+		resultset.next();
+		System.out.println(resultset.getRow());
+		
+		resultset.first();
+		
+		System.out.println(resultset.getRow());
+		
+		resultset.close();
+		statement.close();
 		connection.close();
 	}
 	
