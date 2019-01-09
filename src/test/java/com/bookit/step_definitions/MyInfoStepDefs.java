a28bd43b59b73d6af905ff43eed5ab7a60715ebc
ozzy <ozzy@cybertekschool.com>
2019-01-09 15:53:41 -0500

    jdbc test

--- src/test/java/com/bookit/step_definitions/MyInfoStepDefs.java
+++ src/test/java/com/bookit/step_definitions/MyInfoStepDefs.java
@@ -3,12 +3,16 @@ package com.bookit.step_definitions;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 import com.bookit.pages.SelfPage;
 import com.bookit.pages.SignInPage;
+import com.bookit.pages.TeamPage;
 import com.bookit.utilities.BrowserUtils;
 import com.bookit.utilities.ConfigurationReader;
 import com.bookit.utilities.DBUtils;
@@ -78,6 +82,64 @@ public class MyInfoStepDefs {
 		Assert.assertEquals(actualFullName, expectedFullname);
 		Assert.assertEquals(actualRole, expectedRole);
 
+		
+	}
+	
+	@When("user is on the my team page")
+	public void user_is_on_the_my_team_page() {
+		   SelfPage selfPage = new SelfPage();
+		   selfPage.goToTeam();
+		   BrowserUtils.waitFor(2);
+	}
+
+	@Then("team info should match with the database records {string}")
+	public void team_info_should_match_with_the_database_records(String email) {
+	   	
+		//writing the query 
+		String query = "SELECT firstname,role FROM users\n" + 
+				"WHERE team_id = (SELECT team_id FROM users WHERE email='"+email+"')";
+		
+		//assign query result to list of maps 
+		List<Map<String,Object>> result = DBUtils.getQueryResultMap(query);
+		
+		//-----------------UI
+		//import teampage and create teamPage object to reach names and roles from UI
+		TeamPage teamPage = new TeamPage();
+		
+		//empty list for actual names
+		List<String> actualNames = new ArrayList<>();
+		
+		for(WebElement el: teamPage.teamMemberNames) {
+			//get each webelement name and add to actualNames list 
+			actualNames.add(el.getText());			
+		}
+		
+		//empty list for actual roles
+		List<String> actualRoles = new ArrayList<>();
+		
+		for(WebElement el: teamPage.teamMemberRoles) {
+			//get each webelement roles and add to actualRoles list
+			actualRoles.add(el.getText());
+		}
+		
+		//before one by one comparison, check the number of results from DB and UI
+		Assert.assertEquals(result.size(), actualNames.size());
+		
+		//compare DB names against to UI names(List to List)
+		for(Map<String,Object> row : result) {
+			String firstName = (String) row.get("firstname");
+			
+			Assert.assertTrue(actualNames.contains(firstName));
+		}
+		
+		//compare DB names against to UI roles(List to List)
+		for(Map<String, Object> row : result) {
+            String role = (String) row.get("role");
+            
+            Assert.assertTrue(actualRoles.contains(role));
+        }
+		
+		
 		
 	}
 	
